85494ea0e95b8a995f01fa36f87fb72f45c37ffb
khutangle <khutangle@users.noreply.github.com>
2025-11-08 16:42:55 +0000

    Add account-backend.js, analytics-build.tsx

---
+++ frontend/web/src/styles-dynamic/account-backend.js
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export const pages_manager = () => { return null; };
+
+
---
+++ frontend/web/src/styles-dynamic/analytics-build.tsx
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export default function module_factory_styles() { return <div>Component</div>; }
+
+
