a36d186046dd9aeb822bf4f5e184e2b2a7aa6119
khutangle <khutangle@users.noreply.github.com>
2025-11-08 13:44:12 +0000

    Add routes-model.java, simple-ape-styles.toml

---
+++ frontend/web/src/decorator-constants/routes-model.java
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/medvaultfhe/frontend/web/src/decorator-constants/routes-model.java
+// Generated at: 2025-12-03T23:45:46.852Z
+
+
